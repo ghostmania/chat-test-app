fd9b26a4aa42c009699c887db6ea38777306d228
ghostmania <ghostmania2@gmail.com>
2017-05-24 17:57:32 +0300

    fb button added

--- app/config/main.controller.js
+++ app/config/main.controller.js
@@ -8,10 +8,20 @@
 function MainController(authService) {
     var vm = this;
     vm.logOut = logOut;
+    vm.FB = FB;
+    vm.getUserInfo = getUserInfo;
 
     //functions
     function logOut() {
         authService.logOut();
+    }
+
+    function FB() {
+        authService.watchLoginChange();
+    }
+
+    function getUserInfo() {
+        authService.getUserInfo();
     }
 }
 })();
--- app/services/auth.service.js
+++ app/services/auth.service.js
@@ -5,12 +5,14 @@
         .module("myApp")
         .service("authService", authService);
 
-function authService($state, chatService) {
+function authService($state, chatService, $rootScope) {
         var vm = this;
         vm.userLogin = userLogin;
         vm.logOut = logOut;
+        vm.watchLoginChange = watchLoginChange;
+        vm.getUserInfo = getUserInfo;
 
-        //functions
+    //functions
         function userLogin(userInput) {
             if (userInput){
                 localStorage.setItem('currentUser', userInput);
@@ -19,10 +21,43 @@ function authService($state, chatService) {
         }
 
         function logOut() {
+
             chatService.stop();
             chatService.messages = [];
             localStorage.setItem('currentUser', "");
             $state.go('login');
+        }
+
+        function watchLoginChange() {
+
+            // var _self = this;
+
+            FB.Event.subscribe('auth.authResponseChange', function(res) {
+
+                if (res.status === 'connected') {
+
+                    console.log('The user is already logged, is possible retrieve his personal info')
+                    vm.getUserInfo();
+                    console.log(res.authResponse)
+
+                }
+                else {
+                    console.log('The user is not logged to the app, or into Facebook: destroy the session on the server.')
+                }
+
+            });
+        }
+
+        function getUserInfo() {
+
+            var _self = this;
+
+            FB.api('/me', function(res) {
+                $rootScope.$apply(function() {
+                    $rootScope.user = _self.user = res;
+                });
+            });
+
         }
 }
 })();
