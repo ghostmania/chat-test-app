00350ed3d927c57f7eb664fb9156318a3f2d3c0e
ghostmania <ghostmania2@gmail.com>
2017-05-30 09:53:58 +0300

    added simple FB info fetching

--- app/services/auth.service.js
+++ app/services/auth.service.js
@@ -47,7 +47,7 @@ function authService($state, chatService) {
 
     function testAPI() {
         console.log('Welcome!  Fetching your information.... ');
-        FB.api('/me', function(response) {
+        FB.api('/me?fields=name,email', function(response) {
             console.log(response);
             console.log('Successful login for: ' + response.name);
             console.log(response.email)
